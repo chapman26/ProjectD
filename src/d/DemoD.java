0be1296acf14ef77eebf8069ae70ad6857258145
samchapman <samchapman@192.168.1.245>
2024-01-08 00:02:23 -0500

    This is really cool

---
+++ src/d/DemoD.java
@@ -0,0 +1,14 @@
+package d;
+
+public class DemoD {
+	
+	public static void main(String[] args) {
+
+		System.out.println("Hello again D");
+		
+		System.out.println("This has been fun...");
+		
+		System.out.println("Can we keep learning...");
+	}
+
+}
